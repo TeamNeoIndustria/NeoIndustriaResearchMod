836992a2fff3c4bbf80d56eda6ff847aa5008143
NOVVAH <h_az_2000@hotmail.co.uk>
2026-04-13 13:51:18 +0100

    Added Neolib functions for API

---
+++ src/main/java/net/torchednova/researchmod/api/ResearchAPI.java
@@ -0,0 +1,51 @@
+package net.torchednova.researchmod.api;
+
+import net.torchednova.researchmod.research.Research;
+
+import java.util.List;
+
+public class ResearchAPI {
+
+	public static List<Research> getAvailableResearches()
+	{
+		return null;
+	}
+
+	public static List<Research> getResearchTree()
+	{
+		return null;
+	}
+
+	public static List<Research> getCompleteResearches()
+	{
+		getResearchTree();
+		return null;
+	}
+
+	public static void getResearchProgress()
+	{
+
+	}
+
+	public static Research getCurrentResearch()
+	{
+		return null;
+	}
+
+	public static void setResearch()
+	{
+
+	}
+
+	public static void updateResearchProgress()
+	{
+
+	}
+
+
+
+
+
+
+
+}
